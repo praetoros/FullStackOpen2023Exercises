4aecb6f6261d6b4674ff66ed94a7e873df9117af
Tom Alle <development@praetoros.com>
2023-01-28 22:12:23 +1100

    unicafe step6

--- unicafe/src/App.js
+++ unicafe/src/App.js
@@ -1,40 +1,55 @@
 import {useState} from 'react'
 
 const App = () => {
-    const anecdotes = [
-        'If it hurts, do it more often.',
-        'Adding manpower to a late software project makes it later!',
-        'The first 90 percent of the code accounts for the first 10 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
-        'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
-        'Premature optimization is the root of all evil.',
-        'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
-        'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.',
-        'The only way to go fast, is to go well.'
-    ]
+    // save clicks of each button to its own state
+    const [good, setGood] = useState(0)
+    const [neutral, setNeutral] = useState(0)
+    const [bad, setBad] = useState(0)
 
-    const randomAnecdote = () => {
-        setSelected(Math.floor(Math.random() * anecdotes.length))
-    }
-
-    const vote = (index) => {
-        let tempVotes = [...votes]
-        tempVotes[index] += 1
-        setVotes(tempVotes)
-    }
-
-    const [selected, setSelected] = useState(0)
-    const [votes, setVotes] = useState(new Array(anecdotes.length).fill(0))
-    
     return (
         <div>
-            <h1>Anecdote of the Day</h1>
-            {anecdotes[selected]}
-            <br/>
-            <button onClick={() => randomAnecdote()}>Next Anecdote</button>
-            <button onClick={() => vote(selected)}>Vote</button>
-            <h1>Anecdote with most votes</h1>
-            {anecdotes[votes.indexOf(Math.max(...votes))]}
+            <h1>Give Feedback</h1>
+            <FeedbackButton text="Good" onClick={() => setGood(good + 1)}/>
+            <FeedbackButton text="Neutral" onClick={() => setNeutral(neutral + 1)}/>
+            <FeedbackButton text="Bad" onClick={() => setBad(bad + 1)}/>
+            <h1>Statistics</h1>
+            <Statistics good={good} neutral={neutral} bad={bad}/>
         </div>
+    )
+}
+
+const Statistics = ({good, neutral, bad}) => {
+    if (good + neutral + bad === 0) {
+        return (
+            <p>No feedback given</p>
+        )
+    }
+    return (
+        <table>
+            <tbody>
+            <StatisticDisplay text="Good:" value={good}/>
+            <StatisticDisplay text="Neutral:" value={neutral}/>
+            <StatisticDisplay text="Bad:" value={bad}/>
+            <StatisticDisplay text="All:" value={good + neutral + bad}/>
+            <StatisticDisplay text="Average:" value={(good - bad) / (good + neutral + bad)}/>
+            <StatisticDisplay text="Positive:" value={good / (good + neutral + bad) * 100 + "%"}/>
+            </tbody>
+        </table>
+    )
+}
+
+const FeedbackButton = ({text, onClick}) => {
+    return (
+        <button onClick={onClick}>{text}</button>
+    )
+}
+
+const StatisticDisplay = ({text, value}) => {
+    return (
+        <tr>
+            <td>{text}</td>
+            <td>{value}</td>
+        </tr>
     )
 }
 
