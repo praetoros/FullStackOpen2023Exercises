4e063e664f8de6816e2713e75462ee45841988e5
Tom Alle <development@praetoros.com>
2023-01-28 17:51:59 +1100

    1.12

--- exercise1/src/App.js
+++ exercise1/src/App.js
@@ -1,55 +1,29 @@
 import {useState} from 'react'
 
 const App = () => {
-    // save clicks of each button to its own state
-    const [good, setGood] = useState(0)
-    const [neutral, setNeutral] = useState(0)
-    const [bad, setBad] = useState(0)
+    const anecdotes = [
+        'If it hurts, do it more often.',
+        'Adding manpower to a late software project makes it later!',
+        'The first 90 percent of the code accounts for the first 10 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+        'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+        'Premature optimization is the root of all evil.',
+        'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+        'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.',
+        'The only way to go fast, is to go well.'
+    ]
+
+    const randomAnecdote = () => {
+        setSelected(Math.floor(Math.random() * anecdotes.length))
+    }
+    
+    const [selected, setSelected] = useState(0)
 
     return (
         <div>
-            <h1>Give Feedback</h1>
-            <FeedbackButton text="Good" onClick={() => setGood(good + 1)}/>
-            <FeedbackButton text="Neutral" onClick={() => setNeutral(neutral + 1)}/>
-            <FeedbackButton text="Bad" onClick={() => setBad(bad + 1)}/>
-            <h1>Statistics</h1>
-            <Statistics good={good} neutral={neutral} bad={bad}/>
+            {anecdotes[selected]}
+            <br/>
+            <button onClick={() => randomAnecdote()}>Next Anecdote</button>
         </div>
-    )
-}
-
-const Statistics = ({good, neutral, bad}) => {
-    if (good + neutral + bad === 0) {
-        return (
-            <p>No feedback given</p>
-        )
-    }
-    return (
-        <table>
-            <tbody>
-            <StatisticDisplay text="Good:" value={good}/>
-            <StatisticDisplay text="Neutral:" value={neutral}/>
-            <StatisticDisplay text="Bad:" value={bad}/>
-            <StatisticDisplay text="All:" value={good + neutral + bad}/>
-            <StatisticDisplay text="Average:" value={(good - bad) / (good + neutral + bad)}/>
-            <StatisticDisplay text="Positive:" value={good / (good + neutral + bad) * 100 + "%"}/>
-            </tbody>
-        </table>
-    )
-}
-
-const FeedbackButton = ({text, onClick}) => {
-    return (
-        <button onClick={onClick}>{text}</button>
-    )
-}
-
-const StatisticDisplay = ({text, value}) => {
-    return (
-        <tr>
-            <td>{text}</td>
-            <td>{value}</td>
-        </tr>
     )
 }
 
