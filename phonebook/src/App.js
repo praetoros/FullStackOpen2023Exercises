80929fceeb23c721ba6e0733490b513284036665
Tom Alle <development@praetoros.com>
2023-02-05 21:13:41 +1100

    2.6

--- phonebook/src/App.js
+++ phonebook/src/App.js
@@ -6,20 +6,38 @@ const App = () => {
     ])
     const [newName, setNewName] = useState('')
 
+    const handleAddName = (event) => {
+        event.preventDefault()
+        const personObject = {
+            name: newName
+        }
+        setPersons(persons.concat(personObject))
+        setNewName('')
+    }
+
+    const handleChangeName = (event) => {
+        setNewName(event.target.value)
+    }
+
     return (
         <div>
             <h2>Phonebook</h2>
             <form>
                 <div>
-                    name: <input/>
+                    name:
+                    <input
+                        value={newName}
+                        onChange={handleChangeName}
+                    />
                 </div>
                 <div>
-                    <button type="submit">add</button>
+                    <button type="submit" onClick={handleAddName}>add</button>
                 </div>
             </form>
             <h2>Numbers</h2>
-            ...
+            {persons.map(person => <div key={person.name}>{person.name}</div>)}
         </div>
+
     )
 }
 
